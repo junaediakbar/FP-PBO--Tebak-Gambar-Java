32f268342bb801f3c3cc08136028adea209ae792
Junaedi Akbar <juned.akb@gmail.com>
2021-01-09 19:43:58 +0800

    first

---
+++ src/tebakgambar/TebakGambar.java
@@ -0,0 +1,16 @@
+
+package tebakgambar;
+public class TebakGambar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        start st = new start();
+        st.setVisible(true);
+        
+        
+    }
+    
+}
